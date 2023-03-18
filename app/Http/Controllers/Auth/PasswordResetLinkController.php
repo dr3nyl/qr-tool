d4a058f13085f1480d55cea2012605f673ecf513
dre <drenyltobi@gmail.com>
2023-03-18 14:31:38 +0800

    --PasswordResetLinkController.php-- * Check if secret code is correct when attempt by user * Removed codes related to email
    --auth.php--
    * remove require of token when accessing reset password page

--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -29,16 +29,8 @@ class PasswordResetLinkController extends Controller
             'secret_code' => ['required'],
         ]);
 
-        // We will send the password reset link to this user. Once we have attempted
-        // to send the link, we will examine the response then see the message we
-        // need to show to the user. Finally, we'll send out a proper response.
-        $status = Password::sendResetLink(
-            $request->only('email')
-        );
-
-        return $status == Password::RESET_LINK_SENT
-                    ? back()->with('status', __($status))
-                    : back()->withInput($request->only('email'))
-                            ->withErrors(['email' => __($status)]);
+        return $request['secret_code'] == 'sheeesh!' 
+            ? redirect(route('password.reset')) 
+            : back()->withErrors(['secret_code' => 'Whoops! Invalid secret code.']);
     }
 }
