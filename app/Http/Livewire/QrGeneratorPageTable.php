45b3bfbe5fd252f89a5db72fcba11acb678349ee
dre <drenyltobi@gmail.com>
2023-03-19 13:19:40 +0800

    --tailwind.config.js-- * Add strategy class to avoid any layout conflicts --QrGeneratorPageTable-- * Create powergrid table for qr QrGeneratorPageTable Page --app.blade.php-- * Add livewire and powergrid styles and scripts

---
+++ app/Http/Livewire/QrGeneratorPageTable.php
@@ -0,0 +1,219 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\QrDetail;
+use Illuminate\Support\Carbon;
+use Illuminate\Database\Eloquent\Builder;
+use PowerComponents\LivewirePowerGrid\Rules\{Rule, RuleActions};
+use PowerComponents\LivewirePowerGrid\Traits\ActionButton;
+use PowerComponents\LivewirePowerGrid\{Button, Column, Exportable, Footer, Header, PowerGrid, PowerGridComponent, PowerGridEloquent};
+
+final class QrGeneratorPageTable extends PowerGridComponent
+{
+    use ActionButton;
+
+    /*
+    |--------------------------------------------------------------------------
+    |  Features Setup
+    |--------------------------------------------------------------------------
+    | Setup Table's general features
+    |
+    */
+    public function setUp(): array
+    {
+        $this->showCheckBox();
+
+        return [
+            Exportable::make('export')
+                ->striped()
+                ->type(Exportable::TYPE_XLS, Exportable::TYPE_CSV),
+            Header::make()->showSearchInput(),
+            Footer::make()
+                ->showPerPage()
+                ->showRecordCount(),
+        ];
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    |  Datasource
+    |--------------------------------------------------------------------------
+    | Provides data to your Table using a Model or Collection
+    |
+    */
+
+    /**
+    * PowerGrid datasource.
+    *
+    * @return Builder<\App\Models\QrDetail>
+    */
+    public function datasource(): Builder
+    {
+        return QrDetail::query();
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    |  Relationship Search
+    |--------------------------------------------------------------------------
+    | Configure here relationships to be used by the Search and Table Filters.
+    |
+    */
+
+    /**
+     * Relationship search.
+     *
+     * @return array<string, array<int, string>>
+     */
+    public function relationSearch(): array
+    {
+        return [];
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    |  Add Column
+    |--------------------------------------------------------------------------
+    | Make Datasource fields available to be used as columns.
+    | You can pass a closure to transform/modify the data.
+    |
+    | ❗ IMPORTANT: When using closures, you must escape any value coming from
+    |    the database using the `e()` Laravel Helper function.
+    |
+    */
+    public function addColumns(): PowerGridEloquent
+    {
+        return PowerGrid::eloquent()
+            ->addColumn('id')
+            ->addColumn('part_number')
+
+           /** Example of custom column using a closure **/
+            ->addColumn('part_number_lower', function (QrDetail $model) {
+                return strtolower(e($model->part_number));
+            })
+
+            ->addColumn('date_code')
+            ->addColumn('vendor_code')
+            ->addColumn('qr_code')
+            ->addColumn('created_by')
+            ->addColumn('created_at_formatted', fn (QrDetail $model) => Carbon::parse($model->created_at)->format('d/m/Y H:i:s'))
+            ->addColumn('updated_at_formatted', fn (QrDetail $model) => Carbon::parse($model->updated_at)->format('d/m/Y H:i:s'));
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    |  Include Columns
+    |--------------------------------------------------------------------------
+    | Include the columns added columns, making them visible on the Table.
+    | Each column can be configured with properties, filters, actions...
+    |
+    */
+
+     /**
+     * PowerGrid Columns.
+     *
+     * @return array<int, Column>
+     */
+    public function columns(): array
+    {
+        return [
+            Column::make('ID', 'id')
+                ->makeInputRange(),
+
+            Column::make('PART NUMBER', 'part_number')
+                ->sortable()
+                ->searchable()
+                ->makeInputText(),
+
+            Column::make('DATE CODE', 'date_code')
+                ->sortable()
+                ->searchable()
+                ->makeInputText(),
+
+            Column::make('VENDOR CODE', 'vendor_code')
+                ->sortable()
+                ->searchable()
+                ->makeInputText(),
+
+            Column::make('QR CODE', 'qr_code')
+                ->sortable()
+                ->searchable()
+                ->makeInputText(),
+
+            Column::make('CREATED BY', 'created_by')
+                ->sortable()
+                ->searchable()
+                ->makeInputText(),
+
+            Column::make('CREATED AT', 'created_at_formatted', 'created_at')
+                ->searchable()
+                ->sortable()
+                ->makeInputDatePicker(),
+
+            Column::make('UPDATED AT', 'updated_at_formatted', 'updated_at')
+                ->searchable()
+                ->sortable()
+                ->makeInputDatePicker(),
+
+        ]
+;
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | Actions Method
+    |--------------------------------------------------------------------------
+    | Enable the method below only if the Routes below are defined in your app.
+    |
+    */
+
+     /**
+     * PowerGrid QrDetail Action Buttons.
+     *
+     * @return array<int, Button>
+     */
+
+    /*
+    public function actions(): array
+    {
+       return [
+           Button::make('edit', 'Edit')
+               ->class('bg-indigo-500 cursor-pointer text-white px-3 py-2.5 m-1 rounded text-sm')
+               ->route('qr-detail.edit', ['qr-detail' => 'id']),
+
+           Button::make('destroy', 'Delete')
+               ->class('bg-red-500 cursor-pointer text-white px-3 py-2 m-1 rounded text-sm')
+               ->route('qr-detail.destroy', ['qr-detail' => 'id'])
+               ->method('delete')
+        ];
+    }
+    */
+
+    /*
+    |--------------------------------------------------------------------------
+    | Actions Rules
+    |--------------------------------------------------------------------------
+    | Enable the method below to configure Rules for your Table and Action Buttons.
+    |
+    */
+
+     /**
+     * PowerGrid QrDetail Action Rules.
+     *
+     * @return array<int, RuleActions>
+     */
+
+    /*
+    public function actionRules(): array
+    {
+       return [
+
+           //Hide button edit for ID 1
+            Rule::button('edit')
+                ->when(fn($qr-detail) => $qr-detail->id === 1)
+                ->hide(),
+        ];
+    }
+    */
+}
